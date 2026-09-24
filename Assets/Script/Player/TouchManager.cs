a9b5376d749fb5cfc68716962f0ec50b47742582
DRATHAH <xavier@petersonfoundation.com>
2026-09-24 10:46:03 -0400

    Touch Detection for Grid

--- Assets/Script/Player/TouchManager.cs
+++ Assets/Script/Player/TouchManager.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class TouchManager : MonoBehaviour
 {
     InputActionMap actions;
+    bool isTouching = false;
 
     private void OnEnable()
     {
@@ -26,15 +28,26 @@ public class TouchManager : MonoBehaviour
     void Update()
     {
         // Checks if a touch device exists and a press just started
-        if (Touchscreen.current.primaryTouch.press.wasPressedThisFrame)
+        if (Touchscreen.current.touches.Count > 0)
         {
             // Get screen position of the touch
             Vector2 touchPos = Touchscreen.current.primaryTouch.position.ReadValue();
 
+            bool touchInProgress = false;
+            // Checks if the finger is still touching the screen
+            foreach(Touch touch in Touch.activeTouches)
+            {
+                if (touch.isInProgress)
+                {
+                    touchInProgress = true;
+                }
+            }
+            isTouching = touchInProgress;
+
             // Create a ray from the camera to the touch position
             Ray ray = Camera.main.ScreenPointToRay(touchPos);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit) && isTouching)
             {
                 if (hit.transform.GetComponent<Tile>())
                 {
